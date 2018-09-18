e4062b4cc837421d0d72cc82a58bd2e77dd82769
Nick Bellizzi <ngb3@illinois.edu>
2018-09-18 18:16:49 -0500

    Added word counter and unique word counter code.

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,38 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class WebScraper {
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+
     public static void main(String[] unused) {
-        System.out.println("Ran");
+        String text = urlToString("http://erdani.com/tdpl/hamlet.txt");
+        System.out.println(text);
+        String[] words = text.split(" ");
+        System.out.println("Total words: " + words.length);
+        for (int i = 0; i < words.length; i++) {
+            words[i] = words[i].toLowerCase();
+        }
+        Set set = new HashSet<String>(Arrays.asList(words));
+        System.out.println("Unique words: " + set.size());
     }
 }
